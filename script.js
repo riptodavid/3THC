4e161373c6eed9181d66ba9f5e9b2b292106fbb7
riptodavid <davidmoreno52498@gmail.com>
2024-07-15 17:14:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function scrollToSection(sectionId) {
     document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
 }
+
+function toggleDetails(phaseId) {
+    const details = document.getElementById(`${phaseId}-details`);
+    if (details.style.display === "none") {
+        details.style.display = "block";
+    } else {
+        details.style.display = "none";
+    }
+}
